c421b616b7f06315c76291db1cd5080628a56e64
Chimit <chimit@gmail.com>
2020-07-31 16:21:25 +0800

    Redis drafts

--- storage.js
+++ storage.js
@@ -1,10 +1,18 @@
+const { v4: uuidv4 } = require('uuid');
 const mysql = require('mysql')
+const redis = require('redis')
 
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
     database: 'rolo_tasks'
+})
+
+const redisClient = redis.createClient()
+
+redisClient.on('error', function (err) {
+    console.log('Error ' + err)
 })
 
 function createUser(params) {
@@ -15,7 +23,7 @@ function createUser(params) {
                 
                 reject(error)
             } else {
-                console.log('User is saved!')
+                console.log('User is saved.')
 
                 resolve({ id: results.insertId, ...params });
             }
@@ -25,33 +33,82 @@ function createUser(params) {
 
 function createTask(params) {
     return new Promise((resolve, reject) => {
-        let CURRENT_TIMESTAMP = { toSqlString: function () { return 'CURRENT_TIMESTAMP()'; } };
-        
-        connection.query('INSERT INTO tasks SET ?', { ...params, created_at: CURRENT_TIMESTAMP }, (error, results, fields) => {
+        let uuid = uuidv4()
+
+        redisClient
+            .multi()
+            .hmset('task:' + uuid, params)
+            .expireat('task:' + uuid, parseInt((+new Date) / 1000) + 86400)
+            .exec((error, replies) => {
+                if (error) {
+                    console.log(error)
+
+                    reject(error)
+                } else {
+                    resolve({ uuid: uuid, ...params })
+                }
+            })
+    })
+}
+
+function confirmTask(uuid) {
+    return getTaskFromDrafts(uuid)
+        .then(task => putTaskIntoQueue(task))
+        .then(task => {
+            removeTaskFromDrafts(task.uuid)
+
+            return task
+        })
+}
+
+function getTaskFromDrafts(uuid) {
+    return new Promise((resolve, reject) => {
+        redisClient.hgetall('task:' + uuid, (error, value) => {
             if (error) {
-                console.log('Error saving task!')
+                console.log(value)
 
                 reject(error)
+            } else if (!value) {
+                reject(new Error('Draft not found!'))
             } else {
-                console.log('Task is saved!')
-
-                resolve({ id: results.insertId, ...params });
+                resolve({ uuid, ...value })
             }
         })
     })
 }
 
-function confirmTask(taskId) {
+function removeTaskFromDrafts(uuid) {
     return new Promise((resolve, reject) => {
-        connection.query('UPDATE tasks SET confirmed = ? WHERE id = ?', [1, taskId], (error, results, fields) => {
+        redisClient.del('task:' + uuid, (error, count) => {
             if (error) {
-                console.log('Error updating task!')
+                reject(error)
+            } else if (count === 0) {
+                reject(new Error('Error deleting draft!'))
+            } else {
+                console.log('Draft is successfully deleted.')
+
+                resolve()
+            }
+        })
+    })
+}
+
+function putTaskIntoQueue(task) {
+    return new Promise((resolve, reject) => {
+        let CURRENT_TIMESTAMP = { toSqlString: () => 'CURRENT_TIMESTAMP()' }
+
+        connection.query('INSERT INTO tasks SET ?', {
+            ...task,
+            created_at: CURRENT_TIMESTAMP
+        }, (error, results, fields) => {
+            if (error) {
+                console.log('Error putting the task into the queue!')
 
                 reject(error)
             } else {
-                console.log('Task is updated!')
+                console.log('Task is put into the queue.')
 
-                resolve({ id: results.insertId, ...params });
+                resolve({ id: results.insertId, ...task });
             }
         })
     })
